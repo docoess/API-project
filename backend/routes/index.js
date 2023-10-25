f69eac7386ce8fbecb5cd68322cd447862936f0a
Donald Roessler <me@donaldroessler.com>
2023-10-25 13:58:50 -0700

    Add API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 router.get('/api/csrf/restore', (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie('XSRF-TOKEN', csrfToken);
-  res.status(200).join({
+  res.status(200).json({
     'XSRF-TOKEN': csrfToken
   });
 });
