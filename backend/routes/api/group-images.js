2deb7923ff77d1e3a047576e913dd4bab1e15e19
Donald Roessler <me@donaldroessler.com>
2023-10-31 09:31:42 -0700

    Add delete group image endpoint

---
+++ backend/routes/api/group-images.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const { GroupImage, Group, Membership } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth.js')
+
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+  const userId = req.user.id;
+  const { imageId } = req.params;
+
+  const groupImage = await GroupImage.findByPk(imageId);
+
+  if (!groupImage) {
+    res.status(404);
+    return res.json({
+      message: "Group Image couldn't be found"
+    });
+  }
+
+  const group = await Group.findByPk(groupImage.groupId);
+  const memStatus = await Membership.findOne({
+    where: {
+      userId
+    }
+  });
+
+  if (group.organizerId === userId || memStatus.status === 'co-host') {
+    await groupImage.destroy();
+  }
+
+  return res.json({
+    message: 'Successfully deleted'
+  });
+});
+
+module.exports = router;
