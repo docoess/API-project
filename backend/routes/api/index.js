c983a6c8c3963ec0d67d1a61ad256774f1f273d1
Donald Roessler <me@donaldroessler.com>
2023-10-31 13:15:38 -0700

    Add delete image from event endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const groupsRouter = require('./groups.js');
 const venuesRouter = require('./venues.js');
 const eventsRouter = require('./events.js');
 const groupImagesRouter = require('./group-images.js');
+const eventImagesRouter = require('./event-images.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -15,6 +16,7 @@ router.use('/groups', groupsRouter);
 router.use('/venues', venuesRouter);
 router.use('/events', eventsRouter);
 router.use('/group-images', groupImagesRouter);
+router.use('/event-images', eventImagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
