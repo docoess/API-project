e74c1764a5b32f98cf3f8599f48b289ba0110ee2
Donald Roessler <me@donaldroessler.com>
2023-11-01 09:28:01 -0700

    Add rest of event endpoints

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { Event, Attendance, Group, Venue, EventImage, Membership } = require('../../db/models');
+const { Event, Attendance, Group, Venue, EventImage, Membership, User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth.js')
 
 const { check } = require('express-validator');
@@ -72,6 +72,79 @@ router.get('/:eventId', async (req, res, next) => {
   });
 
   return res.json(event);
+
+});
+
+router.get('/:eventId/attendees', async (req, res, next) => {
+  let userId;
+  if (req.user) {
+    userId = req.user.id;
+  }
+
+  const { eventId } = req.params;
+
+  const event = await Event.findByPk(eventId);
+  if (!event) {
+    res.status(404);
+    return res.json({
+      message: "Event couldn't be found"
+    });
+  }
+
+  const group = await Group.findByPk(event.groupId);
+  let organizer;
+  let cohost;
+
+  if (userId) {
+    organizer = group.organizerId === userId;
+
+    const membership = await Membership.findOne({
+      where: {
+        groupId: group.id,
+        userId
+      }
+    });
+
+    if (membership) {
+      cohost = membership.status === 'co-host';
+    }
+  }
+
+  const attendees = {"Attendees": []};
+
+  let attendances;
+
+  if (organizer || cohost) {
+    attendances = await Attendance.findAll({
+      where: {
+        eventId
+      }
+    });
+  } else {
+    attendances = await Attendance.findAll({
+      where: {
+        eventId,
+        status: {
+          [Op.in]: ['attending', 'waitlist']
+        }
+      }
+    });
+  }
+
+  for (let attendance of attendances) {
+    const user = await User.findByPk(attendance.userId);
+
+    attendees.Attendees.push({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      Attendance: {
+        status: attendance.status
+      }
+    });
+  }
+
+  return res.json(attendees);
 
 });
 
@@ -180,6 +253,85 @@ router.post('/:eventId/images', requireAuth, async (req, res, next) => {
   });
 
   return res.json(eventImage);
+});
+
+router.post('/:eventId/attendance', requireAuth, async (req, res, next) => {
+  const userId = req.user.id;
+  const { eventId } = req.params;
+
+  const event = await Event.findByPk(eventId);
+  if (!event) {
+    res.status(404);
+    return res.json({
+      message: "Event couldn't be found"
+    });
+  }
+
+  const attendance = await Attendance.findOne({
+    where: {
+      userId,
+      eventId
+    }
+  });
+
+  if (attendance) {
+    res.status(400);
+
+    if (attendance.status === 'pending') {
+      return res.json({
+        message: "Attendance has already been requested"
+      });
+    } else {
+      return res.json({
+        message: "User is already an attendee of the event"
+      });
+    }
+  } else {
+    await Attendance.create({
+      eventId,
+      userId,
+      status: 'pending'
+    });
+  }
+
+  return res.json({
+    userId,
+    status: 'pending'
+  });
+
+});
+
+router.put('/:eventId/attendance', requireAuth, async (req, res, next) => {
+  const userId = req.user.id;
+  const { eventId } = req.params;
+
+  let event = await Event.findByPk(eventId);
+
+  if (!event) {
+    res.status(404);
+    return res.json({
+      message: "Event couldn't be found"
+    });
+  }
+
+  const changedUserId = req.body.userId;
+  const changedStatus = req.body.status;
+
+  if (changedStatus === 'pending') {
+    res.status(400);
+    return res.json({
+      message: "Cannot change an attendance status to pending"
+    });
+  }
+
+  const group = await Group.findByPk(event.groupId);
+  const organizer = group.organizerId === userId;
+  const membership = await Membership.findOne({
+    groupId: group.id,
+    userId
+  });
+  const cohost = membership.status === 'co-host';
+
 });
 
 router.put('/:eventId', requireAuth, validateEvent, async (req, res, next) => {
